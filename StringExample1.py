ea7c39cc7682c152024ba9115bd219ae228d2c16
Shailender Kumar <Shailender@tests-MacBook-Pro.local>
2018-09-26 21:00:34 +0530

    Update

---
+++ StringExample1.py
@@ -0,0 +1,12 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+print(parrot[3])
+print(parrot[0])
+print(parrot[-1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
+print(parrot[-4:-2])
+print(parrot[0:4:2])
+print(parrot[0:6:3])
